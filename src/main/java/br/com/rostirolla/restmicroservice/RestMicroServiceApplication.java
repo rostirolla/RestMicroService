3853706cf19f940b169713121f465803208b4a79
Carlos Henrique de Souza Rostirolla <rostirolla@gmail.com>
2020-03-14 19:20:46 -0300

    Commit inicial.

---
+++ src/main/java/br/com/rostirolla/restmicroservice/RestMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.rostirolla.restmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rostirolla/restmicroservice/RestMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rostirolla.restmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
