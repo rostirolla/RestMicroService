8e9a721849a6f7c4ae2f8179363d89680ef0fcd4
carlos-souza-telecon <carlos.souza.telecon@gmail.com>
2021-04-22 17:44:40 -0300

    Tentativa de ativar o CORS na api

---
+++ src/main/java/br/com/rostirolla/restmicroservice/CorsConfig.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfig {
+
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurerAdapter() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+
+}
